4b474d91c14103fab59cd46d3638872cef6a3627
balaji <balubongu185@gmail.com>
2026-07-15 22:38:21 +0530

    roboshop dev infra

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  ami              = data.aws_ami.joindevops.id
+  ami      = data.aws_ami.joindevops.id
   sg_id    = data.aws_ssm_parameter.sg_id.value
    subnet=[
     data.aws_ssm_parameter.private_subnet_a.value,
@@ -8,7 +8,7 @@ locals {
     frontend_listener_arn    = data.aws_ssm_parameter.frontend_listener_arn.value
     backend_listener_arn    = data.aws_ssm_parameter.backend_listener_arn.value
     listener = "${var.components}" == "frontend" ? local.frontend_listener_arn : local.backend_listener_arn
-    host_header = "${var.components}" == "frontend" ? "${var.project}.${var.env}.${var.domain_name}" :"${var.components}.backend-alb-${var.env}.${var.domain_name}" 
+    host_header = "${var.components}" == "frontend" ? "${var.project}-${var.env}.${var.domain_name}" :"${var.components}.backend-alb-${var.env}.${var.domain_name}" 
     port = "${var.components}" == "frontend" ? 80 : 8080
     path= "${var.components}" == "frontend" ? "/" : "/health"
     component="${var.components}"
